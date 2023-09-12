05435b09619631564727e9aa2d7eadbeb80b155a
febrian-irv <febrian.irv@gmail.com>
2023-09-12 21:05:16 +0700

    nitip-barang initialization

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'nama': 'Febrian Irvansyah',
+        'kelas': 'PBP-A'
+    }
+
+    return render(request, "main.html", context)
